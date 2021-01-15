3eaedd92af55b993799bf1cab8429e61fcba6ce1
unknown <camilleborges167@gmail.com>
2021-01-15 19:09:07 -0300

    Video Angular 2

---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent implements OnInit {
+
+  items: Product[];
+
+  constructor(private service: ProductsService) { }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(){
+    this.service.getAll().subscribe((data: any) => this.items = data);
+  }
+
+  delete(id: number){
+    this.service.delete(id).subscribe(
+      (data: any) => this.callbackSuccess(),
+      (error: any) => this.callbackError(error)
+    );
+  }
+
+  private callbackSuccess(){
+    alert('Registro excluído com sucesso');
+  }
+
+  private callbackError(error: any){
+    alert('Ocorreu um problema ao excluir');
+    console.log(error);
+  }
+
+}
